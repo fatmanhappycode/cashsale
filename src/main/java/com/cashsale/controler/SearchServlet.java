853e008115a6d176183c74b6468c68a3ffced9e4
fatmanhappycode <1943279276@qq.com>
2018-10-27 00:52:00 +0800

    .

--- src/main/java/com/cashsale/controler/SearchServlet.java
+++ src/main/java/com/cashsale/controler/SearchServlet.java
@@ -2,6 +2,7 @@ package com.cashsale.controler;
 
 import com.alibaba.fastjson.JSONObject;
 import com.cashsale.bean.Customer;
+import com.cashsale.bean.Product;
 import com.cashsale.bean.Result;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -16,6 +17,11 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author 肥宅快乐码
@@ -30,15 +36,40 @@ public class SearchServlet extends HttpServlet {
 
         String searchByTitle = req.getParameter("searchByTitle");
         JsonObject jsonObject = (JsonObject) new JsonParser().parse(searchByTitle);
-        String title = "";
+        String title = jsonObject.get("title").getAsString();
+        int currentPage = jsonObject.get("currentPage").getAsInt();
+
+        currentPage = (currentPage - 1) * 8;
 
         PrintWriter writer = resp.getWriter();
         Connection conn = new com.cashsale.conn.Conn().getCon();
 
         try{
-            PreparedStatement pstmt = conn.prepareStatement("SELECT INTO product_info WHERE title LIKE %?%");
+            PreparedStatement pstmt = conn.prepareStatement("SELECT INTO product_info WHERE title LIKE %?% LIMIT ?,8");
             pstmt.setString(1,title);
+            pstmt.setInt(2,currentPage);
             ResultSet rs = pstmt.executeQuery();
+            ResultSetMetaData metaData = rs.getMetaData();
+            int cols_len = metaData.getColumnCount();
+            List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+            while (rs.next()) {
+                Map<String, Object> map = new HashMap<String, Object>();
+                for (int i = 0; i < cols_len; i++) {
+                    String cols_name = metaData.getColumnName(i + 1);
+                    Object cols_value = rs.getObject(cols_name);
+                    if (cols_value == null) {
+                        cols_value = "";
+                    }
+                    map.put(cols_name, cols_value);
+                }
+                list.add(map);
+            }
+            if (list != null) {
+                for (Map<String, Object> map : list) {
+                    Product p = new Product(map);
+                    result.add(s);
+                }
+            }
 
             writer.print(JSONObject.toJSON(new Result<String>(107,null,"查询成功")));
         } catch (Exception e) {
