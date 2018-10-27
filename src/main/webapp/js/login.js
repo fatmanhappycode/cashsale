bef289783e840a63746b0f8fb77336169012c28c
fatmanhappycode <1943279276@qq.com>
2018-10-28 00:44:12 +0800

    login.js

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -3,29 +3,29 @@ alert(myUrl);*/
 var token="";
 function loadXMLDoc()
 {
-	var username = $("#username").val();
-	var password = $("#password").val();
-	var saveData={"username":username,"password":password};
-	
-	$.ajax({
-		url:"/login",
-		type:"post",
-		dataType:"json",
-		data:JSON.stringify(saveData),
-		contentType:"application/json",
-		success:function(result,testStatus)
-		{
-			token=result.data;
-			//alert(result.msg);
-			if(result.code== "105"&& token!=""){
-				window.location.href="index.html?token="+token;
-			}else if(result.code== "106"){
-				alert(result.msg);
-			}
-		},
-		error:function(xhr,errrorMessage,e){
-			alert("系统异常！");
-		}
-	}); 
-	  
+    var username = $("#username").val();
+    var password = $("#password").val();
+    var saveData={"username":username,"password":password};
+
+    $.ajax({
+        url:"/login",
+        type:"post",
+        dataType:"json",
+        data:JSON.stringify(saveData),
+        contentType:"application/json",
+        success:function(result,testStatus)
+        {
+            token=result.data;
+            //alert(result.msg);
+            if(result.code== "105"&& token!=""){
+                window.location.href="index.html?token="+token;
+            }else if(result.code== "106"){
+                alert(result.msg);
+            }
+        },
+        error:function(xhr,errrorMessage,e){
+            alert("系统异常！");
+        }
+    });
+
 }
