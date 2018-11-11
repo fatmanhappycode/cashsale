f4c965f28c1dff71c8bebdd85adeef0355328504
fatmanhappycode <1943279276@qq.com>
2018-11-11 17:03:40 +0800

    .

--- src/main/java/com/cashsale/dao/RegisterDAO.java
+++ src/main/java/com/cashsale/dao/RegisterDAO.java
@@ -7,6 +7,10 @@ import java.sql.ResultSet;
 import com.cashsale.util.MailUtil;
 import com.cashsale.util.TimeUtil;
 
+/**
+ * @author Sylvia
+ * 2018年11月3日
+ */
 public class RegisterDAO {
 	
 	/** 提示进行邮箱激活的code */
@@ -18,28 +22,35 @@ public class RegisterDAO {
 	/** 邮箱已被注册 */
 	private static final int EMAIL_IS_REGISTER = 110;
 	
+	private Connection conn = new com.cashsale.conn.Conn().getCon();
+	private PreparedStatement pstmt = null;
+	private PreparedStatement pstmt2 = null;
+	private PreparedStatement pstmt3 = null;
+	private ResultSet rs = null;
+	private ResultSet rs2 = null;
+	private ResultSet rs3 = null;
 	
 	public int register(String username, String email, String encodedPass, String encodedCode, String nickname,
 			String code, String password){
 		
-		Connection conn = new com.cashsale.conn.Conn().getCon();
-        
         try {
         	//判断该用户名和邮箱是否已被注册
-            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM user_data WHERE user_name=?");
+        	pstmt = conn.prepareStatement("SELECT * FROM user_data WHERE user_name=?");
             pstmt.setString(1,username);
-            ResultSet rs = pstmt.executeQuery();
-            PreparedStatement pstmt2 = conn.prepareStatement("SELECT * FROM all_user WHERE user_name=?");
+            rs = pstmt.executeQuery();
+            pstmt2 = conn.prepareStatement("SELECT * FROM all_user WHERE user_name=?");
             pstmt2.setString(1, username);
-            ResultSet rs2 = pstmt2.executeQuery();
-            PreparedStatement pstmt3 = conn.prepareStatement("SELECT * FROM user_data WHERE email = ?");
+            rs2 = pstmt2.executeQuery();
+            pstmt3 = conn.prepareStatement("SELECT * FROM user_data WHERE email = ?");
             pstmt3.setString(1, email);
-            ResultSet rs3 = pstmt3.executeQuery();
+            rs3 = pstmt3.executeQuery();
             
             if (rs.next() || rs2.next())  {
+            	closeConn();
                 return NUMBER_IS_REGISTER;
             } 
             else if(rs3.next()){
+            	closeConn();
             	return EMAIL_IS_REGISTER;
             }
             else{
@@ -50,22 +61,36 @@ public class RegisterDAO {
 				pstmt2.setString(3, nickname);
 				pstmt2.setString(4, email);
 				pstmt2.setString(5, code);
-
-				//获取当前时间
+            	
+            	String sql = "ALTER TABLE commodity_score ADD a"+username+" VARCHAR(10) DEFAULT 0";
+            	pstmt3 = conn.prepareStatement(sql);
+            	
+            	//获取当前时间
             	String currentTime = TimeUtil.getTime();
                 
             	//发送邮件
             	MailUtil.sendMail(email, encodedCode, username, encodedPass, nickname, currentTime);
             	
             	pstmt.execute();
-            	pstmt2.executeUpdate();
+            	pstmt2.execute();
+            	pstmt3.executeUpdate();
+            	
+            	closeConn();
             	return ACTIVATION_TIP;
             	//writer.print(JSONObject.toJSON(new ResultDTO<String>(109, null, "请到邮箱进行账号激活！")));
             }
         } 
         catch (Exception e) {
+        	closeConn();
         	e.printStackTrace();
             return REGISTER_FAILED;
         }
+	}
+	
+	/** 关闭所有链接  */
+	public void closeConn() {
+		new com.cashsale.conn.Conn().closeConn(rs, pstmt, conn);
+    	new com.cashsale.conn.Conn().closeConn(rs2, pstmt2, conn);
+    	new com.cashsale.conn.Conn().closeConn(rs3, pstmt3, conn);
 	}
 }
