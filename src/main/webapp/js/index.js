d4fed7df8fe23e1c0ab681b67cd1bfb181b0037b
fatmanhappycode <1943279276@qq.com>
2018-11-16 18:05:27 +0800

    .

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,11 +1,6 @@
 function goodsclick(str) {
     var goods=str.childNodes;
     var productId=goods[3].value;
-<<<<<<< HEAD
-=======
-    setCookie("productId",productId);
-    var saveData={"productId":productId};
->>>>>>> 758d44a44ff6b7f4730c993b256496d710ee246e
     // alert(productId);
     var string="goods.html?productId="+productId
     window.open(string);
