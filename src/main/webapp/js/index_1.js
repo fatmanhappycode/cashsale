675dc487aaeb97a9aa38603db74fb91130ea7409
fatmanhappycode <1943279276@qq.com>
2018-11-16 11:52:15 +0800

    1

--- src/main/webapp/js/index_1.js
+++ src/main/webapp/js/index_1.js
@@ -6,6 +6,11 @@ isLoading=false;
 currentPage='';
 //定义全局变量title，方便下拉时使用
 var title;
+function whatULike() {
+    var play = document.getElementById("play");
+    var img
+    play.appendChild()
+}
 function innerGoods(data) {
 	$.each(data,function(index,obj){
 		var main = document.getElementById("main");
@@ -208,6 +213,7 @@ function loadXMLDoc()
 			if(result.code== "107"){
                 $(window).scrollTop(250);
 				innerGoods(data);
+                document.documentElement.scrollTop = 800;
 			}else{
 				console.log("查询失败！");
 			}
@@ -380,6 +386,7 @@ function Selectspecies() {
             data=result.data.data;
             if(result.code== "107"){
                 innerGoods(data);
+                document.documentElement.scrollTop = 800;
             }else{
                 console.log("查询失败！");
             }
