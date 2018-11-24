834c644d992d99e775dce0250d735a19104b61d8
8-0416 <916061509@qq.com>
2018-11-24 16:42:26 +0800

    update status code

--- src/main/java/com/cashsale/dao/ActiveDAO.java
+++ src/main/java/com/cashsale/dao/ActiveDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import com.cashsale.enums.ResultEnum;
 import com.cashsale.util.TimeUtil;
 
 /**
@@ -12,55 +13,49 @@ import com.cashsale.util.TimeUtil;
  */
 public class ActiveDAO {
 
-	/** 验证码过期的code */
-	private static final int CODE_IS_EXPIRE = 113;
-	/** 激活成功 */
-	private static final int ACTIVE_SUCCESSED = 101;
-	/** 验证码有误 */
-	private static final int CODE_IS_WRONG = 111;
-	/** 验证失败 */
-	private static final int VERTIFY_FAILED = 112;
-
 	private Connection conn = new com.cashsale.conn.Conn().getCon();
 	private PreparedStatement pstmt = null;
 	private PreparedStatement pstmt2 = null;
+	private PreparedStatement pstmt3 = null;
 	private ResultSet result = null;
 	private ResultSet result2 = null;
 
-	public int active(String code, String currentTime, String username, String password) {
+	public int active(String code, String currentTime, String username/*, String password*/) {
 		try {
 			//根据激活码查询用户
 			pstmt = conn.prepareStatement("SELECT user_name FROM register_user WHERE code = ?");
 			pstmt.setString(1, code);
 			result = pstmt.executeQuery();
 			//根据用户名查询密码
-			pstmt2 = conn.prepareStatement("SELECT pass_word FROM register_user WHERE user_name = ? ");
+			/*pstmt2 = conn.prepareStatement("SELECT pass_word FROM register_user WHERE user_name = ? ");
 			result2 = null;
 
 			if( result.next() )
 			{
 				pstmt2.setString(1, result.getString(1));
 				result2 = pstmt2.executeQuery();
-			}
+			}*/
 
-			//若根据验证码找得到该用户，且密码正确，时间未超过五分钟，则验证通过
-			if( result2.next() || result2.getString(1).equals(password) )
+			//若根据验证码找得到该用户，且用户名正确，时间未超过五分钟，则验证通过
+			if( result.next() && result.getString("user_name").equals(username) )
 			{
+				System.out.println(1);
 				//判断验证时间是否超过五分钟，若超过则删除该用户的注册信息
 				if( TimeUtil.emailTime(currentTime) )
 				{
+					System.out.println(2);
 					pstmt = conn.prepareStatement("SELECT * FROM register_user WHERE code = ?");
 					pstmt.setString(1, code);
 					result = pstmt.executeQuery();
 
 					pstmt2 = conn.prepareStatement("INSERT INTO user_data(user_name, credit, nick_name, email) VALUES (?,600,?,?)");
-					PreparedStatement pstmt3 = conn.prepareStatement("INSERT INTO all_user(user_name,pass_word) VALUES (?,?)");
+					pstmt3 = conn.prepareStatement("INSERT INTO all_user(user_name,pass_word) VALUES (?,?)");
 					if (result.next()) {
 						pstmt2.setString(1,result.getString("user_name"));
 						pstmt2.setString(2,result.getString("nick_name"));
 						pstmt2.setString(3,result.getString("email"));
-						pstmt3.setString(1,result.getString(1));
-						pstmt3.setString(2,result.getString(2));
+						pstmt3.setString(1,result.getString("user_name"));
+						pstmt3.setString(2,result.getString("pass_word"));
 						pstmt2.executeUpdate();
 						pstmt3.executeUpdate();
 					}
@@ -71,23 +66,23 @@ public class ActiveDAO {
 					pstmt2.execute();
 
 					closeConn();
-					return ACTIVE_SUCCESSED;
+					return ResultEnum.ACTIVE_SUCCESS.getCode();
 				}
 				else
 				{
 					closeConn();
-					return CODE_IS_EXPIRE;
+					return ResultEnum.ACTIVE_TIME_ERROR.getCode();
 				}
 			}
 			else
 			{
 				closeConn();
-				return CODE_IS_WRONG;
+				return ResultEnum.ACTIVE_ERROR.getCode();
 			}
 		} catch(Exception e) {
 			e.printStackTrace();
 			closeConn();
-			return VERTIFY_FAILED;
+			return ResultEnum.ERROR.getCode();
 		}
 	}
 
